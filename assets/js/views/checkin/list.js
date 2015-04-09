4e21a35c2fbd374722f2fbd5da8e81d49d2f6a7b
Quentin Ville <ville.quentin@gmail.com>
2015-04-09 10:14:37 +0200

    Commit : ajout de la carte qui fonctionne qui est affichée au dessus de la liste des checkins.

--- assets/js/views/checkin/list.js
+++ assets/js/views/checkin/list.js
@@ -4,12 +4,14 @@ define(
         'underscore',
         'backbone',
         'collections/checkins',
-        'text!./../../../templates/checkin/list.html' // le plugin text permet de récupérer le template
+        'text!./../../../templates/checkin/list.html', // le plugin text permet de récupérer le template
+        'async!http://maps.google.com/maps/api/js?sensor=false'
     ], function($,_,Backbone,CheckInCollection, checkinListTemplate){
 
         var CheckInListView = Backbone.View.extend({
             el: '#content',
             template : _.template(checkinListTemplate),
+            map: {},
 
             render: function(){
                 console.log('CheckInListView Render');
@@ -20,17 +22,26 @@ define(
                         console.log(self.$el);
                         console.log(checkins.models);
 
+                        self.$el.html(self.template({
+                            checkInList: checkins.models
+                        })); 
+
                         _.each(checkins.models, function (checkin){
                             console.log(checkin.get('lat'));
                             console.log(checkin.get('lng'));
+
                         });
 
-
-                        self.$el.html(self.template({
-                            checkInList: checkins.models 
-                        }));
+                        this.map = new google.maps.Map(
+                            document.getElementById('map-canvas'),{
+                            zoom:16,
+                            center: new google.maps.LatLng(43.81451767218152, -91.25057458877563),
+                            mapTypeId: google.maps.MapTypeId.ROADMAP
+                        });       
                         
                     }
+
+
                 })
             }
 
