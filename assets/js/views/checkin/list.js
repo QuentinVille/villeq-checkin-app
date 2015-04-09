ba71fc6100468544d00e9c49fc0a6673c9a8d562
Quentin Ville <ville.quentin@gmail.com>
2015-04-09 10:42:24 +0200

    Commit : ajout de la fonction d'affichage des checkins à partir de markers dans la Gmap

--- assets/js/views/checkin/list.js
+++ assets/js/views/checkin/list.js
@@ -11,7 +11,11 @@ define(
         var CheckInListView = Backbone.View.extend({
             el: '#content',
             template : _.template(checkinListTemplate),
-            map: {},
+            map: null,
+            myLatlng: null,
+            mapOptions: null,
+            markers: [],
+            marker: null,
 
             render: function(){
                 console.log('CheckInListView Render');
@@ -26,18 +30,39 @@ define(
                             checkInList: checkins.models
                         })); 
 
+                        // Ici il faut ajouter les coordonnées récupérés qui proviennet de l'ordinateur
+                        myLatlng = new google.maps.LatLng(45.1747598,5.7071189); 
+
+                        // Définition des paramètres par défaut d'affichage de la carte
+                        mapOptions = {
+                            zoom: 12,
+                            center: myLatlng
+                        }
+
+                        // Génération de la carte dans la view
+                        map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+                        // Parcours de l'ensemble des checkins et création des markers
                         _.each(checkins.models, function (checkin){
                             console.log(checkin.get('lat'));
                             console.log(checkin.get('lng'));
 
+                            checkinLatlng = new google.maps.LatLng(checkin.get('lat'),checkin.get('lng')); 
+
+                            marker = new google.maps.Marker({
+                                position: checkinLatlng,
+                                map: map,
+                                title: 'MyCheckin with id='+checkin.get('id')
+                            });
+
                         });
 
-                        this.map = new google.maps.Map(
-                            document.getElementById('map-canvas'),{
-                            zoom:16,
-                            center: new google.maps.LatLng(43.81451767218152, -91.25057458877563),
-                            mapTypeId: google.maps.MapTypeId.ROADMAP
-                        });       
+                        // this.map = new google.maps.Map(
+                        //     document.getElementById('map-canvas'),{
+                        //     zoom:16,
+                        //     center: new google.maps.LatLng(43.81451767218152, -91.25057458877563),
+                        //     mapTypeId: google.maps.MapTypeId.ROADMAP
+                        // });       
                         
                     }
 
